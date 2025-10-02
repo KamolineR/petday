ec0c1cee84abb85faa9eff714bd2910637ff7d22
KamolineR <kamoline.araujo@escola.pr.gov.br>
2025-10-02 17:22:46 -0300

    ajustamento e criação de pastas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-const express = require('express')
 const conexao = require('./config/database')
 const bodyparser = require('body-parser')
-
-const app = express() // criação do servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando');
-});
 
 conexao.authenticate()  // conexão com o banco
     .then(() =>{
@@ -16,7 +10,5 @@ conexao.authenticate()  // conexão com o banco
         console.error('Erro ao tentar conectar com o banco de dados')
     });
 
-
-// ROTAS E LÓGICA
 
 
