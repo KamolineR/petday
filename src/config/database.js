2596f760e2010d5a8bb4352ec9b077391cc846b8
KamolineR <164529564+KamolineR@users.noreply.github.com>
2025-10-24 09:41:41 -0300

    Delete src directory

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-const conexao = new Sequelize('dbpetday', 'root', 'escola', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
-
-module.export = conexao;
